235df80e6f01f33f34ebc57e90862ac383e04687
drumil32 <akheniad@gmail.com>
2024-11-15 21:53:05 +0530

    till 15-11-2024

---
+++ M3/Code/Lect 20 Project Class/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      lineHeight:{
+        lg: '5rem'
+      }
+    },
+  },
+  plugins: [],
+}
+
