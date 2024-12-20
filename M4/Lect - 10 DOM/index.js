9309ab9f949f5064cadde704e74993319eed7f27
drumil32 <akheniad@gmail.com>
2024-12-20 21:53:39 +0530

    till 20-12-2024

---
+++ M4/Lect - 10 DOM/index.js
@@ -0,0 +1,46 @@
+// const headTag = document.querySelector('h1');
+
+// headTag.setAttribute('style', 'background-color: blue;');
+
+// // style="background-color: blue;"
+
+// // headTag.innerText = headTag.innerText + ' How are you?';
+
+// // innerText
+// // innerHTML
+// console.log(headTag.innerHTML);
+// // hello <i>How are you?</i>
+
+// console.log(headTag.innerText);
+// // hello How are you?
+
+
+// // headTag.innerText = '<u>How are you?</u>';
+// {/* 
+//     <p>
+//         <b>How are you?</b>
+//         <i>How are you?</i>
+//     </p>
+// */}
+
+// // headTag.setAttribute('style', 'font-size: 20px;');
+// // style="font-size: 20px;"
+// headTag.style.fontSize = '20px';
+// console.log(headTag);
+
+// console.log(headTag.textContent)
+// console.log(headTag.innerText)
+// console.log(headTag.innerHTML)
+
+
+// Task 
+const divTag = document.querySelector('div');
+
+divTag.style.width = '300px';
+divTag.style.height = '300px';
+divTag.style.backgroundColor = 'yellow';
+divTag.innerText = "Hello, I'm a div!";
+divTag.style.color = 'blue';
+divTag.style.fontSize = '20px';
+divTag.style.fontWeight = 'bold';
+divTag.style.padding = '20px';
