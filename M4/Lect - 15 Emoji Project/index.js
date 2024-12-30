8a34f25d69f295f81fa5fd35e84a7760dda24b48
drumil32 <akheniad@gmail.com>
2024-12-30 20:52:43 +0530

    till 30-12-2024

---
+++ M4/Lect - 15 Emoji Project/index.js
@@ -0,0 +1,58 @@
+const tbody = document.querySelector('tbody');
+
+(() => {
+    emojiList
+        .forEach((item, ind) => {
+            const row = document.createElement('tr');
+
+            const index = document.createElement('td');
+            index.innerText = ind;
+            const emojiCell = document.createElement('td');
+            emojiCell.innerText = item.emoji;
+            const aliaseCell = document.createElement('td');
+            aliaseCell.innerText = item.aliases.join(', ')
+            const descriptionCell = document.createElement('td');
+            descriptionCell.innerText = item.description;
+
+            row.appendChild(index);
+            row.appendChild(emojiCell);
+            row.appendChild(aliaseCell);
+            row.appendChild(descriptionCell);
+
+
+            tbody.appendChild(row);
+        })
+})();
+
+const searchTag = document.querySelector('#search');
+
+searchTag.addEventListener('input', (e) => {
+    const searchText = e.target.value.toLowerCase();
+
+    tbody.innerHTML = '';
+
+    emojiList.forEach((item,ind) => {
+        if (item.description.includes(searchText) ||
+            item.aliases.includes(searchText) ||
+            item.tags.includes(searchText)) {
+
+            const row = document.createElement('tr');
+
+            const index = document.createElement('td');
+            index.innerText = ind;
+            const emojiCell = document.createElement('td');
+            emojiCell.innerText = item.emoji;
+            const aliaseCell = document.createElement('td');
+            aliaseCell.innerText = item.aliases.join(', ')
+            const descriptionCell = document.createElement('td');
+            descriptionCell.innerText = item.description;
+
+            row.appendChild(index);
+            row.appendChild(emojiCell);
+            row.appendChild(aliaseCell);
+            row.appendChild(descriptionCell);
+
+            tbody.appendChild(row);
+        }
+    });
+});
