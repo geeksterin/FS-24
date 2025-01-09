a6da46ab1d5a4900cd136c7dd2d3cf78c2be493c
drumil32 <akheniad@gmail.com>
2025-01-09 21:59:42 +0530

    till 09-01-2025

---
+++ M4/Lect - 21 Storage/index.js
@@ -0,0 +1,45 @@
+// sessionStorage.setItem('firstName', 'Drumil');
+// sessionStorage.setItem('lastName', 'Akheania');
+
+
+// const person = {
+//     firstName: 'Azit',
+//     lastName: 'Rana'
+// }
+
+// console.log(person + '');
+// sessionStorage.setItem('person', person);
+// sessionStorage.setItem('person-strinify', JSON.stringify(person));
+
+// console.log(sessionStorage.getItem('person-strinify'));
+// console.log(JSON.parse(sessionStorage.getItem('person-strinify')));
+
+
+// // setTimeout(() =>
+// //     sessionStorage.removeItem('lastName')
+// //     , 2_000);
+
+
+// // setTimeout(() => sessionStorage.clear(), 5_000);
+
+
+
+// localstorage
+    // - size is 5MB
+    // - not secured
+    // it will persist the data till the browser is open
+    // setItem, getItem, removeItem, clear
+    // key and value both should be strings
+
+// sessionStorage
+    // - size is 5MB
+    // - secured
+    // it will persist the data till the tab is open inside browser
+    // setItem, getItem, removeItem, clear
+    // key and value both should be strings
+
+// cookies
+    // size is 4KB
+    // secured
+    // to save user authentication information
+    
