a126fae5ef0d49572c27101c2e3c24133970f9c7
drumil32 <akheniad@gmail.com>
2025-01-09 22:14:57 +0530

    till 09-01-2025

--- M4/Lect - 21 Storage/index.js
+++ M4/Lect - 21 Storage/index.js
@@ -25,21 +25,57 @@
 
 
 // localstorage
-    // - size is 5MB
-    // - not secured
-    // it will persist the data till the browser is open
-    // setItem, getItem, removeItem, clear
-    // key and value both should be strings
+// - size is 5MB
+// - not secured
+// it will persist the data till the browser is open
+// setItem, getItem, removeItem, clear
+// key and value both should be strings
 
 // sessionStorage
-    // - size is 5MB
-    // - secured
-    // it will persist the data till the tab is open inside browser
-    // setItem, getItem, removeItem, clear
-    // key and value both should be strings
+// - size is 5MB
+// - secured
+// it will persist the data till the tab is open inside browser
+// setItem, getItem, removeItem, clear
+// key and value both should be strings
 
 // cookies
-    // size is 4KB
-    // secured
-    // to save user authentication information
-    
+// size is 4KB
+// secured
+// to save user authentication information
+
+
+
+
+
+
+
+
+const takeDataFromUser = () => {
+    const firstName = prompt('give your first name');
+    const lastName = prompt('give your last name');
+    const age = prompt('give your age');
+    if (firstName == null || lastName == null || isNaN(age)) {
+        alert('Invalid input');
+    } else {
+        localStorage.setItem('firstName', firstName);
+        localStorage.setItem('lastName', lastName);
+        localStorage.setItem('age', age);
+        alert('Data saved successfully');
+    }
+}
+
+const renderData = () => {
+    const keyArr = ['firstName', 'lastName', 'age'];
+
+    const valueList = document.querySelectorAll('.value');
+    valueList.forEach((valueTag, index) => {
+        valueTag.innerHTML = localStorage.getItem(keyArr[index]);
+    })
+}
+
+if (!localStorage.getItem('firstName')) {
+    takeDataFromUser();
+}
+
+renderData();
+
